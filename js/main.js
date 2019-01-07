c3d0d1d693f81e48e996d0923dc7a9f85b76dcda
Salar <morning684@gmail.com>
2019-01-07 11:32:55 +0330

    main.js updated with proxy example

--- js/main.js
+++ js/main.js
@@ -3,10 +3,38 @@ let person = {
     family : "ESM",
     pro: () => {
         doing();
+    },
+    age: () => {
+        let y = new Date().getFullYear();
+        return ( y - 1990);  
     }
 }
 function doing() {
     console.log("I'm coding!");
 }
 
+console.log(person.age());
 person.pro();
+/////////proxy example:
+
+let proxyPerson = new Proxy(person, {
+    get(target, property) {
+        if (property == "age")
+            return `age: ${target[property]()}`;
+        else
+            return target[property];
+    },
+    set(target, property, value) {
+        if (property == "family") {
+            return (console.log("not allowed to set family"));
+        } else {
+            return target[property] = value;
+        }
+    }
+});
+
+console.log(proxyPerson.age);
+proxyPerson.family = "Mehr";
+console.log(proxyPerson.family);
+proxyPerson.name = "Nima";
+console.log(proxyPerson.name);
